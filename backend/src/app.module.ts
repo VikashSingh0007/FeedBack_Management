513175332bf0b4ea6ff480e782f1643ba3beaaba
vsingh <vsingh@asurint.com>
2025-07-31 21:55:16 +0530

    added cloud db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { FeedbackModule } from './feedback/feedback.module';
@@ -12,16 +12,35 @@ import { CategoriesModule } from './categories/categories.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +(process.env.DATABASE_PORT ?? 5432),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    TypeOrmModule.forRootAsync({
+  useFactory: (config: ConfigService) => {
+    const isProd = config.get<string>('NODE_ENV') === 'production';
+    const dbUrl = config.get<string>('DATABASE_URL');
+
+    return dbUrl
+      ? {
+          type: 'postgres',
+          url: dbUrl,
+          autoLoadEntities: true,
+          synchronize: !isProd,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }
+      : {
+          type: 'postgres',
+          host: config.get<string>('DATABASE_HOST'),
+          port: config.get<number>('DATABASE_PORT'),
+          username: config.get<string>('DATABASE_USER'),
+          password: config.get<string>('DATABASE_PASSWORD'),
+          database: config.get<string>('DATABASE_NAME'),
+          autoLoadEntities: true,
+          synchronize: !isProd,
+        };
+  },
+  inject: [ConfigService],
+}),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
       serveRoot: '/uploads',
